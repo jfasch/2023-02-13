9044175821911a950fcbba502643b3ce2e262235
Joerg Faschingbauer <jf@faschingbauer.co.at>
2023-02-15 13:44:10 +0100

    implement mycat exercise

--- exercises/mycat-joerg.faschingbauer.cpp
+++ exercises/mycat-joerg.faschingbauer.cpp
@@ -1,5 +1,10 @@
+#include <fcntl.h>
+#include <unistd.h>
+
 #include <iostream>
+#include <string>
 using namespace std;
+
 
 int main(int argc, char** argv)
 {
@@ -8,6 +13,31 @@ int main(int argc, char** argv)
         return 1;
     }
 
-    cerr << "Hier geschieht ein Wunder! " << argv[1] << endl;
+    string filename = argv[1];
+    int fd = open(filename.c_str(), O_RDONLY);
+    if (fd == -1) {
+        perror("open");
+        return 1;
+    }
+
+    while (true) {
+        char buf[16];
+        ssize_t n_bytes_read = read(fd, buf, sizeof(buf));
+        if (n_bytes_read == -1) {
+            perror("read");
+            return 2;
+        }
+
+        if (n_bytes_read == 0) // EOF
+            break;
+
+        ssize_t n_bytes_written = write(STDOUT_FILENO, buf, n_bytes_read);
+        if (n_bytes_written == -1) {
+            perror("write");
+            return 3;
+        }
+    }
+
+    close(fd);
     return 0;
 }
